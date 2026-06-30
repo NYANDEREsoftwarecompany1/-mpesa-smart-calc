0a56e8c15478ea22d967a94a1c811293d1b7a2d1
NYANDEREsoftwarecompany1 <sstanleysstanleyomondijosseph@gmail.com>
2026-06-30 13:53:36 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
-import 'services/ad_service.dart';
+import 'ad_service.dart';
 import 'main_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  
+  await MobileAds.instance.updateRequestConfiguration(
+    RequestConfiguration(
+      testDeviceIds: [], // Add your device ID here after first run
+      maxAdContentRating: MaxAdContentRating.pg,
+    ),
+  );
+  
   await MobileAds.instance.initialize();
-  AdService.loadInterstitialAd();
+  await AdService.init();
   runApp(const MyApp());
 }
 
@@ -18,6 +26,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'M-PESA Smart Calc KE',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        useMaterial3: true,
+      ),
       home: const MainScreen(),
     );
   }
