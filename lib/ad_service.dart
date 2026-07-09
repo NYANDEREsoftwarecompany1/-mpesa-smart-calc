8d0529b3a000f1c8dec2da12f0d1885271fb0a79
NYANDEREsoftwarecompany1 <sstanleysstanleyomondijosseph@gmail.com>
2026-07-10 00:23:56 +0300

    Update ad_service.dart

--- lib/ad_service.dart
+++ lib/ad_service.dart
@@ -1,14 +1,16 @@
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 class AdService {
-  // TEST IDS - REPLACE WITH YOUR REAL IDS BEFORE PUBLISH
-  static const String bannerAdUnitId = 'ca-app-pub-3940256099942544/6300978111';
-  static const String interstitialAdUnitId = 'ca-app-pub-3940256099942544/1033173712';
+  // BANNER - MainScreen Banner
+  static const String bannerAdUnitId = 'ca-app-pub-617926399180966/5661893937';
+  
+  // INTERSTITIAL - Calc Interstitial  
+  static const String interstitialAdUnitId = 'ca-app-pub-617926399180966/8096527141';
 
   static BannerAd createBannerAd(Function onAdLoaded) {
     return BannerAd(
       adUnitId: bannerAdUnitId,
-      request: AdRequest(),
+      request: const AdRequest(),
       size: AdSize.banner,
       listener: BannerAdListener(
         onAdLoaded: (ad) => onAdLoaded(),
@@ -16,13 +18,13 @@ class AdService {
           ad.dispose();
         },
       ),
-    )..load();
+    );
   }
 
   static void loadInterstitialAd(Function(InterstitialAd) onAdLoaded) {
     InterstitialAd.load(
       adUnitId: interstitialAdUnitId,
-      request: AdRequest(),
+      request: const AdRequest(),
       adLoadCallback: InterstitialAdLoadCallback(
         onAdLoaded: onAdLoaded,
         onAdFailedToLoad: (err) => null,
