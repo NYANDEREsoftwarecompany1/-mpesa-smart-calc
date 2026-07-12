75837c164de11e3d82071435efddc0557ebc6723
NYANDEREsoftwarecompany1 <sstanleysstanleyomondijosseph@gmail.com>
2026-07-12 16:19:44 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,20 @@ void main() async {
   runApp(MpesaSmartCalcApp());
 }
 
-class MpesaSmartCalcApp extends StatelessWidget {
+class MpesaSmartCalcApp extends StatefulWidget {
+  @override
+  State<MpesaSmartCalcApp> createState() => _MpesaSmartCalcAppState();
+}
+
+class _MpesaSmartCalcAppState extends State<MpesaSmartCalcApp> {
+  ThemeMode _mode = ThemeMode.system;
+
+  void updateTheme(ThemeMode newMode) {
+    setState(() {
+      _mode = newMode;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,15 +30,21 @@ class MpesaSmartCalcApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
         brightness: Brightness.light,
+        scaffoldBackgroundColor: Color(0xFFF2F8F2),
+        appBarTheme: AppBarTheme(backgroundColor: Color(0xFF00A651), foregroundColor: Colors.white),
+        cardColor: Colors.white,
         useMaterial3: true,
       ),
       darkTheme: ThemeData(
         primarySwatch: Colors.green,
         brightness: Brightness.dark,
+        scaffoldBackgroundColor: Color(0xFF121212),
+        appBarTheme: AppBarTheme(backgroundColor: Color(0xFF1A1A1A), foregroundColor: Colors.white),
+        cardColor: Color(0xFF1E1E1E),
         useMaterial3: true,
       ),
-      themeMode: ThemeMode.system,
-      home: MainScreen(),
+      themeMode: _mode,
+      home: MainScreen(currentMode: _mode, onModeChanged: updateTheme),
     );
   }
 }
